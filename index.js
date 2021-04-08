4055bc8a30e1d3bb72809030da1392ba69d594fb
Jacob <jacobliem1@gmail.com>
2021-04-08 15:04:13 -0500

    commit tree 32431554c6d6e468a11d5b04d61df417abeeafc8

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+   customerName = customerName.toUpperCase();
+}
+
+customerName
+
+var bestCustomer
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+
+bestCustomer
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "jake";
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "ryan";
+}
